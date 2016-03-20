d9382144d46322c292ec1a8722d530a269d37035
gaga <ukugaogao@gmail.com>
2016-03-20 21:45:49 +0800

    v1.1 mooncity

--- gifts/default.js
+++ gifts/default.js
@@ -5,24 +5,25 @@ $(function(){
 	* 从底层节点开始处理
 	*/
 	
-	//处理 4 级 节点
+	// 处理 4 级 节点
 	$(".level4Indent").each(function(i){
-		//处理 5 级节点
+		// 处理 5 级节点
 		copynodeBtonodeAasChild(this,"level5Indent","level5Indents");
-		//绑定事件
-		$(this).click(function(){
-			$(this).find(".level5Indents").toggle();
+		// 绑定事件
+		var level4=$(this);
+		level4.find(".level4Style").click(function(){
+			level4.find(".level5Indents").toggle();
 		});
 	});
-	//处理 3 级 节点
+	// 处理 3 级 节点
 	$(".level3Indent").each(function(i){
 		copynodeBtonodeAasChild(this,"level4Indent","level4Indents");
 	});
-	//处理 2 级 节点
+	// 处理 2 级 节点
 	$(".level2Indent").each(function(i){
 		copynodeBtonodeAasChild(this,"level3Indent","level3Indents");
 	});
-	//处理 1 级 节点
+	// 处理 1 级 节点
 	$(".level1Indent").each(function(i){
 		copynodeBtonodeAasChild(this,"level2Indent","level2Indents");
 	});
@@ -31,31 +32,67 @@ $(function(){
 
 
 	/*
-	*生成辅助导航
+	* 生成辅助导航
 	*/
 
 	var wrapper=$("<div class='wrapper'></div>");
 	var nav=$("<div class='right-nav'></div>");
 	nav.appendTo($("body"));
 	$(".level2Indent").each(function(i){
-		var id="2("+i+")";		//为节点编号 便于索引；
+		var id="2("+i+")";		// 为节点编号 便于索引；
 		$(this).attr({"id":id});
-		//生成一个指向 2级节点 的链接
+		// 生成一个指向 2级节点 的链接
 		$("<a href=#"+id+"></a>").text($(this).find(".level2Style").text()).attr("class","two").css("padding-left","1em").appendTo(wrapper);
 		$(this).find(".level3Indent").each(function(j){
-			var id="2("+i+")"+"3("+j+")";		//为节点编号
+			var id="2("+i+")"+"3("+j+")";		// 为节点编号
 			$(this).attr({"id":id});
-			//生成一个指向 3级节点 的链接
+			// 生成一个指向 3级节点 的链接
 			$("<a href=#"+id+"></a>").text($(this).find(".level3Style").text()).attr("class","three").css("padding-left","2em").appendTo(wrapper);
 			$(this).find(".level4Indent").each(function(k){
-				var id="2("+i+")"+"3("+j+")"+"4("+k+")";		//为节点编号
+				var id="2("+i+")"+"3("+j+")"+"4("+k+")";		// 为节点编号
 				$(this).attr({"id":id});
-				//生成一个指向 4级节点 的链接
+				// 生成一个指向 4级节点 的链接
 				$("<a href=#"+id+"></a>").text($(this).find(".level4Style").text()).attr("class","four").css("padding-left","4em").appendTo(wrapper);
 			})
 		})
 	})
 	wrapper.appendTo(nav);
+
+
+	/*
+	* 控制辅助导航
+	*/
+
+	nav.hide();
+	var btn=$("<div id='btn' class='btn icon-reorder icon-2x'></div>");
+	btn.appendTo($("body"));
+	btn.click(function(){
+		if(btn.attr("class").match(/reorder/)){
+			$(this).attr("class","btn icon-remove icon-2x");
+		}else{
+			$(this).attr("class","btn icon-reorder icon-2x");
+		}
+		nav.toggle();
+	});
+
+
+	/*
+	* 返回顶部导航
+	*/
+
+	var back_to_top=$("<div id='back-to-top' class='btn icon-angle-up icon-2x'></div>");
+	var back_to_bottom=$("<div id='back-to-bottom' class='btn icon-angle-down icon-2x'></div>");
+	back_to_top.appendTo($("body"));
+	back_to_bottom.appendTo($("body"));
+	// 返回顶部代码
+	back_to_top.on('click',function(){
+        	$('body,html').animate({scrollTop:0},500);
+	});
+	// 返回底部代码
+	back_to_bottom.on('click',function(){
+        	$('body,html').animate({scrollTop:document.body.scrollHeight},500);
+	});
+
 })
 
 
@@ -71,8 +108,8 @@ function copynodeBtonodeAasChild(nodeA,nodeB,className){
 	var child=$(nodeA).next();					        // A类型节点的下一个节点
 	while(child.attr("class") == nodeB){
 		var tempEle=child.next();
-		child.appendTo(father);			//将节点添加到节点组中
-		child=tempEle;				    //继续检测下一个节点
+		child.appendTo(father);			// 将节点添加到节点组中
+		child=tempEle;				    // 继续检测下一个节点
 	}
 	father.appendTo(nodeA);
 }
